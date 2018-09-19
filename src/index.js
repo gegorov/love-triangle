7867139abd3af21278e9fdc47c5c0bb3a1f3e166
gegorov <gegorov@yahoo.com>
2018-09-20 00:25:34 +0300

    add solution to love triangles

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i += 1) {
+    const firstLover = preferences[i];
+    const secondLover = preferences[firstLover - 1];
+    const thirdLover = preferences[secondLover - 1];
+
+    if (thirdLover - 1 === i) {
+      count += 1;
+    }
+  }
+  return Math.floor(count / 3);
 };
